93e99f033a623112e354ba75dd59891efbe343b9
Gemirson <43867047+gemirson@users.noreply.github.com>
2020-09-01 15:17:59 -0300

    Feat: Integration Test
    Insert integration test with  specflow

---
+++ DevBoost.Test.Integration/resource/features/User.feature
@@ -0,0 +1,17 @@
+﻿#language: pt-br
+
+Funcionalidade: Validar login
+                Como administrador do sistema
+                Eu quero poder validar o login da aplicação
+                Para que eu possa acessar o sistema
+Esquema do Cenario: Validar login com sucesso
+         Dado um usuario "<usuario>" com o perfil de administrado
+         Quando  preenche o campo senha com a "<senha>"
+         E aciona o envio
+         Então o usuario recebe o statusCode "<statusCode>"
+         
+         Exemplos: 
+         | usuario | senha | statusCode |
+         | jose    | jose  |    200     |
+
+
---
+++ DevBoost.Test.Integration/resource/steps/ValidarLoginSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace DevBoost.Test.Integration.resource.steps
+{
+    [Binding]
+    public class ValidarLoginSteps
+    {
+        [Given(@"um usuario ""(.*)"" com o perfil de administrado")]
+        public void DadoUmUsuarioComOPerfilDeAdministrado(string p0)
+        {            
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"preenche o campo senha com a ""(.*)""")]
+        public void QuandoPreencheOCampoSenhaComA(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"aciona o envio")]
+        public void QuandoAcionaOEnvio()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"o usuario recebe o statusCode ""(.*)""")]
+        public void EntaoOUsuarioRecebeOStatusCode(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
